db029dca996b13e5f94a8d36384841ef69b5ee77
Sindre Andreassen <sindrema@gmail.com>
2021-08-11 00:13:39 +0200

    Initial setup

---
+++ utils/generate.js
@@ -0,0 +1,38 @@
+require("colors");
+const fs = require("fs");
+const templates = require("./templates");
+
+const componentName = process.argv[2];
+
+if (!componentName) {
+    console.error("Please supply a valid component name".red);
+    process.exit(1);
+}
+
+console.log("Creating Component Templates with name: " + componentName);
+
+const componentDirectory = `./src/components/${componentName}`;
+
+if (fs.existsSync(componentDirectory)) {
+    console.error(`Component ${componentName} already exists.`.red);
+    process.exit(1);
+}
+
+fs.mkdirSync(componentDirectory);
+
+const generatedTemplates = templates.map((template) => template(componentName));
+
+generatedTemplates.forEach((template) => {
+    let fileName = componentName
+    if (template.extension === '.tsx') {
+        fileName = 'index'
+    }
+    fs.writeFileSync(
+        `${componentDirectory}/${fileName}${template.extension}`,
+        template.content
+    );
+});
+
+console.log(
+    "Successfully created component under: " + componentDirectory.green
+);
